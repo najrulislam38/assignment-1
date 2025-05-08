89dab29b62ed7794e4501eb9e728ff1e121fb80e
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-05-08 21:04:14 +0600

    added README file, check all fuctions and methods, remove all comments

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
- 
+# TypeScript Interview Questions & Answers
+
+## 6. How does TypeScript help in improving code quality and project maintainability?
+
+- **Static type checking** - Detects type-related errors at compile time instead of runtime, reducing bugs.
+- **Easier refactoring safety**: TypeScript helps us safely rename variables, extract methods, and refactor code without disrupting functionality.
+- **Improved team collaboration**: Code becomes more predictable and Implements consistent interfaces and data structures across large teams or projects.
+- **Better tooling & Integrations**: Powerful types make it easier to manage and grow a codebase, especially in enterprise-level applications. Large applications can be easily maintained using Typescript's autocompletion, refactoring, and navigation.
+- **Self-documenting code**: Types serve as a form of documentation, making code easier to understand without external documentation.
+
+## Provide an example of using union and intersection types in TypeScript.
+
+Union and intersection types are used to compose multiple types into one. They provide more flexibility in typing, enabling you to combine or combine types.
+
+**Union Type:**
+
+```ts
+function userId(id: string | number) {
+  console.log("ID:", id);
+}
+```
+
+**Intersection Type:**
+
+```ts
+type Person = { name: string };
+type Employee = { id: number };
+
+type Staff = Person & Employee;
+
+const employee: Staff = {
+  name: "Alia Hasmit",
+  id: 24561,
+};
+```
--- assignment.ts
+++ assignment.ts
@@ -8,26 +8,13 @@ function formatString(input: string, toUpper?: boolean): string {
   }
 }
 
-// formatString("Bangladesh");
-// formatString("Bangladesh", true);
-// formatString("Bangladesh", false);
-
 function filterByRating(
   items: { title: string; rating: number }[]
 ): { title: string; rating: number }[] {
   const filterItemByRating = items.filter((item) => item?.rating >= 4);
 
-  // console.log(filterRating);
   return filterItemByRating;
 }
-
-const books = [
-  { title: "English for today", rating: 4.0 },
-  { title: "Bangla", rating: 4.5 },
-  { title: "General Math", rating: 3.9 },
-];
-
-filterByRating(books);
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
   let newArray = [];
@@ -38,13 +25,6 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 
   return newArray;
 }
-
-// console.log(
-//   concatenateArrays(["Programming", "Hero"], ["Best of learning skills"])
-// );
-// console.log(concatenateArrays([1, 2], [3, 4], [5], [12]));
-concatenateArrays(["Programming", "Hero"], ["Best of learning skills"]); // Output: ["a", "b", "c"]
-concatenateArrays([1, 2], [3, 4], [5], [20]); // Output: [1, 2, 3, 4, 5]
 
 class Vehicle {
   private make: string;
@@ -75,23 +55,13 @@ class Car extends Vehicle {
 
 const myCar = new Car("Toyota", 2023, "HGD-24343");
 
-// console.log(myCar.getInfo());
-// console.log(myCar.getModel());
-
 function processValue(value: string | number): number {
   if (typeof value === "string") {
-    // console.log(value.length);
-
     return value.length;
   } else {
-    // console.log(value * 2);
-
     return value * 2;
   }
 }
-
-processValue("bangladeshi");
-processValue(10);
 
 interface Product {
   name: string;
@@ -101,22 +71,20 @@ interface Product {
 function getMostExpensiveProduct(products: Product[]): Product | null {
   if (products.length > 0) {
     const mostExpensiveProduct = products.reduce(
-      (previousValue, currentValue) =>
-        currentValue.price > previousValue.price ? currentValue : previousValue
+      (previousProduct, currentProduct) =>
+        currentProduct.price > previousProduct.price
+          ? currentProduct
+          : previousProduct
     );
-    // console.log(mostExpensiveProduct);
+    console.log(mostExpensiveProduct);
 
     return mostExpensiveProduct;
   } else {
-    // console.log(null);
+    console.log(null);
 
     return null;
   }
 }
-
-const products: Product[] = [];
-
-getMostExpensiveProduct(products);
 
 enum Day {
   Monday,
@@ -136,14 +104,6 @@ function getDayType(day: Day): string {
   }
 }
 
-// console.log(getDayType(Day.Sunday));
-// console.log(getDayType(Day.Monday));
-// console.log(getDayType(Day.Tuesday));
-// console.log(getDayType(Day.Wednesday));
-// console.log(getDayType(Day.Thursday));
-// console.log(getDayType(Day.Friday));
-// console.log(getDayType(Day.Saturday));
-
 async function squareAsync(n: number): Promise<number> {
   return new Promise((resolve, reject) => {
     if (n >= 0) {
@@ -155,6 +115,3 @@ async function squareAsync(n: number): Promise<number> {
     }
   });
 }
-
-squareAsync(4).then(console.log); // Output after 1s: 16
-// squareAsync(-3).catch(console.error);
